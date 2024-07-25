81063e364dde50923bdfe3f0cfbbcc164eb05e79
fabrice bellin <fabrice.bellin.formasup82@gmail.com>
2024-07-25 07:23:57 +0200

    correctif3 MonFooter

--- src/MonFooter.js
+++ src/MonFooter.js
@@ -3,7 +3,7 @@ import React from 'react';
 function MonFooter({ prenom, nom, session }) {
     return (
     <footer className="bg-gray-400 text-black p-4 text-xl">
-        <p>{nom} {nom} | {session}</p>
+        <p>{nom} {prenom} | {session}</p>
     </footer>
     );
 }
